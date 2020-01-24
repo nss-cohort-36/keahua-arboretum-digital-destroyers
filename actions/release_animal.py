c6677d4db4b22cc19ae72cfdebaaa470c47fc2bf
Julian Garcia <garcia.j.julian@gmail.com>
2020-01-24 11:32:17 -0600

    added animals displayed to release_animal.py

--- actions/release_animal.py
+++ actions/release_animal.py
@@ -7,8 +7,14 @@ def release_animal(arboretum):
     animal = None
 
     # list of animals that are shouwn to users
-    print("1. River Dolphin")
-    print("2. Dragonfly")
+    print("1. Gold Dust Day Gecko")
+    print("2. River Dolphin")
+    print("3. Nene Goose")
+    print("4. Kikakapu")
+    print("5. Pueo")
+    print("6. Ulae")
+    print("7. Ope'ape'a")
+    print("8. Happy-face Spider")
     # TODO: insert remianing 6 animals
 
     choice = input("Choose animal to release > ")
