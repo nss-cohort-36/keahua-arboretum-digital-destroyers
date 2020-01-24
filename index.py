5f89184e51d1936c80d82fc50d62483c2b0e7890
MelodyStern <50681060+melliemuse@users.noreply.github.com>
2020-01-24 11:05:50 -0600

    Revert "commented out to make sure menu display"

--- index.py
+++ index.py
@@ -4,7 +4,7 @@
 import os
 from arboretum import Arboretum
 from actions.annex import annex_habitat
-# from actions.release_animal import release_animal
+from actions.release_animal import release_animal
 from actions.report import build_facility_report
 
 # It creates a single instance of the class Arboretum
@@ -32,20 +32,20 @@ def main_menu():
     if choice == "1":
         annex_habitat(keahua)
 
-    # if choice == "2":
-    #     release_animal(keahua)
+    if choice == "2":
+        release_animal(keahua)
 
-    # if choice == "3":
-    #     pass
+    if choice == "3":
+        pass
 
-    # if choice == "4":
-    #     pass
+    if choice == "4":
+        pass
 
-    # if choice == "5":
-    #     build_facility_report(keahua)
-    #     pass
+    if choice == "5":
+        build_facility_report(keahua)
+        pass
 
-    # if choice != "6":
-    #     main_menu()
+    if choice != "6":
+        main_menu()
 
 main_menu()
