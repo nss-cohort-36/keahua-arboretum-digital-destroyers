71c4202f106acc383892f85ac142e6c0335f8df2
Julian Garcia <garcia.j.julian@gmail.com>
2020-01-24 13:40:58 -0600

    added the cultivating_new_plant.py to index. option: Add plant to Habitat populates with results. FUNCTIONS DO NOT WORK YET

---
+++ actions/cultivate_new_plant.py
@@ -0,0 +1,15 @@
+import os
+
+def cultivate_new_plant (arboretum):
+    plant = None
+
+    # list of animals that are shouwn to users
+    print("1. Mountain Apple Tree")
+    print("2. Silversword")
+    print("3. Rainbow Eucalyptus Tree")
+    print("4. Blue Jade Vine")
+
+    choice = input("Choose plant to cultivate > ")
+    # logic that creates an instance of the class
+    # if choice == "1":
+    #     plant = MountainAppleTree()
