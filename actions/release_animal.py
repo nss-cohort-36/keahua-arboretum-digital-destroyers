a649e6ea3640e03b10222338af7d46000223d117
Julian Garcia <garcia.j.julian@gmail.com>
2020-01-24 11:20:10 -0600

    got the menu to show by commenting code out on release_animal.py

--- actions/release_animal.py
+++ actions/release_animal.py
@@ -1,5 +1,5 @@
 # should display a list of animals to release
-from animals import RiverDolphin
+# from animals import RiverDolphin
 # TODO: import ofther animals
 
 # Function of chosing animals to release
@@ -13,23 +13,23 @@ def release_animal(arboretum):
 
     choice = input("Choose animal to release > ")
     # logic that creates an instance of the class
-    if choice == "1":
-        animal = RiverDolphin()
+    # if choice == "1":
+    #     animal = RiverDolphin()
     
-    if choice == "2":
-        pass
+    # if choice == "2":
+    #     pass
     #  TODO: finish list of animals
 
     # Iterates & displays through each biome and how many 
     # animals are in the biome
-    for index, river in enumerate(arboretum.rivers):
-        print(f'{index + 1}. River {river.id}')
+    # for index, river in enumerate(arboretum.rivers):
+    #     print(f'{index + 1}. River {river.id}')
 
-    print("Release the animal into which biome?")
-    choice = input("> ")
+    # print("Release the animal into which biome?")
+    # choice = input("> ")
 
     # adds the animal you choose to the biome
-    arboretum.rivers[int(choice) - 1].animals.append(animal)
+    # arboretum.rivers[int(choice) - 1].animals.append(animal)
     #  TODO: Conditional that limits animals in a biome
 
 
