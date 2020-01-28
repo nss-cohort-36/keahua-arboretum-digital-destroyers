3e4842e1e529c185d5dd3fcd808eb2b51f153127
rhc822 <rhc822@gmail.com>
2020-01-28 14:20:33 -0600

    Added Open Field and Tree interfaces

--- interfaces/animal/__init__.py
+++ interfaces/animal/__init__.py
@@ -1,1 +1,2 @@
 # from .freshwater import IFreshwater
+from .open_field import IOpen_Field
---
+++ interfaces/animal/tree.py
@@ -0,0 +1,7 @@
+class ITree:
+
+    def __init__(self):
+        self.dwell_type = "arboreal"
+
+    def run(self):
+        print("The animal walks")
