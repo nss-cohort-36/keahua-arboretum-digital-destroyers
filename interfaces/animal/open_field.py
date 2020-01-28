6400f12c40921df5da5bd19d897aa17a507094cc
rhc822 <rhc822@gmail.com>
2020-01-28 14:33:14 -0600

    Took out unnecessary functions

--- interfaces/animal/open_field.py
+++ interfaces/animal/open_field.py
@@ -2,6 +2,3 @@ class IOpen_Field:
 
     def __init__(self):
         self.open_fields = "open_fields"
-
-    def run(self):
-        print("The animal walks")
--- interfaces/animal/tree.py
+++ interfaces/animal/tree.py
@@ -2,6 +2,3 @@ class ITree:
 
     def __init__(self):
         self.dwell_type = "arboreal"
-
-    def run(self):
-        print("The animal walks")
