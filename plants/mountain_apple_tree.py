c567ecd5983a6a876b811506ef1884d3ccc619bb
Julian Garcia <garcia.j.julian@gmail.com>
2020-01-29 11:28:24 -0600

    addedwhere you can see list of biomes after se;ecting a plant to cultivate

---
+++ plants/mountain_apple_tree.py
@@ -0,0 +1,9 @@
+from interfaces import Identifiable
+
+class MountainAppleTree(Identifiable):
+
+# defines the initial properties of River class
+    def __init__(self):
+      Identifiable.__init__(self)
+
+      self.name = "name"
